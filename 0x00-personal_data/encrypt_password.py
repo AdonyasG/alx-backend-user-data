50c6e7a112678d803622467e910ec76d848280d7
AdonyasG <adonyas2000@gmail.com>
2023-04-27 15:45:10 -0400

    add validator func

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validator"""
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
