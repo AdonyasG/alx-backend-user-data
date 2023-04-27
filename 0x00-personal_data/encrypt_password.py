50c4d85a221ae6876d00c6bb228c531b6f0baf1a
AdonyasG <adonyas2000@gmail.com>
2023-04-27 15:48:59 -0400

    return fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,5 +13,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """validator"""
-    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
-        return True
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password) 
