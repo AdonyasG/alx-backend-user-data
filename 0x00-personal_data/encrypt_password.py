0e5bdd98e67b4a68e5ac9b22f4d7a70195e9b482
AdonyasG <adonyas2000@gmail.com>
2023-04-27 15:34:44 -0400

    type annotation fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,7 +4,7 @@ from typing import ByteString
 import bcrypt
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     """simple password hashing"""
     salt = bcrypt.gensalt()
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
