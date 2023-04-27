d59f6d494b206f85aa2a4277e40fc64b80deab48
AdonyasG <adonyas2000@gmail.com>
2023-04-27 15:29:01 -0400

    type annotated

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """encrypt_password"""
-
+from typing import ByteString
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> ByteString:
     """simple password hashing"""
     salt = bcrypt.gensalt()
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
