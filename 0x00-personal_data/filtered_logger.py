4d820d4d65d582cd66251ba54e5c12e8cb0b7cb6
AdonyasG <adonyas2000@gmail.com>
2023-04-27 09:56:14 -0400

    type annotation added

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """filtered_logger"""
-
+from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """matches certain field and changes the value"""
     for field in fields:
         pattern = r"(?<=\b{}=)[^{}]+".format(field, separator)
