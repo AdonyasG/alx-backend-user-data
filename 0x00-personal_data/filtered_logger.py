7e0930f979cb00380d93e392fe254897e12f53c7
AdonyasG <adonyas2000@gmail.com>
2023-04-27 09:58:51 -0400

    pycodestyle fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """matches certain field and changes the value"""
     for field in fields:
         pattern = r"(?<=\b{}=)[^{}]+".format(field, separator)
