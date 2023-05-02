b214b4b7519535eaac64a273f0397845531595a3
AdonyasG <adonyas2000@gmail.com>
2023-05-02 14:34:39 -0400

    initial commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,17 @@ class Auth():
     """Class Auth"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Requre auth"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        for exclude_path in excluded_paths:
+            if exclude_path.endswith('/'):
+                if path.startswith(exclude_path.rstrip('/')):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
