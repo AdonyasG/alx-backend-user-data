d24ae036faaefae42afbf0f8c446bc32d08cd32b
AdonyasG <adonyas2000@gmail.com>
2023-05-03 09:38:27 -0400

    fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -17,11 +17,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 auth = None
-auth_type = os.getenv("AUTH_TYPE")
+if os.getenv("AUTH_TYPE") is not None:
+    auth = os.getenv("AUTH_TYPE")
 
-if auth_type == 'basic_auth':
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
