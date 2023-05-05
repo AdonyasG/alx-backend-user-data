5053d99266c5f80dda8833a501564a986876d62a
AdonyasG <adonyas2000@gmail.com>
2023-05-04 21:10:19 -0400

    initial commit

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,9 @@ class SessionAuth(Auth):
         sessionId = str(uuid.uuid4())
         self.user_id_by_session_id[sessionId] = user_id
         return sessionId
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        userId = self.user_id_by_session_id.get(session_id)
+        return userId
