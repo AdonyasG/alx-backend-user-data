f8c86e8a5617052609a8f3ba703b4a20ccc4bdaa
AdonyasG <adonyas2000@gmail.com>
2023-05-10 10:49:31 -0400

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,7 +53,9 @@ class Auth:
         """create session"""
         try:
             user = self._db.find_user_by(email=email)
-            if user:
-                return _generate_uuid()
+            session_id = _generate_uuid()
+            user_id = user.id
+            self._db.update_user(user_id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return None
