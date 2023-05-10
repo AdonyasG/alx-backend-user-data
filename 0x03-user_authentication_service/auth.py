a030e002b3f5fee30208c44a7ae00b9825bc8f28
AdonyasG <adonyas2000@gmail.com>
2023-05-10 07:46:00 -0400

    returns hashed password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""encrypt_password"""
+from typing import ByteString
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """simple password hashing"""
+    salt = bcrypt.gensalt()
+    hashed_pw = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_pw
