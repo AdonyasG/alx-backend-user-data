b84686407f02da9d86409d39beeea72ca58ad574
AdonyasG <adonyas2000@gmail.com>
2023-05-10 10:06:26 -0400

    session implmentation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,3 +48,12 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """create a session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return _generate_uuid()
+        except NoResultFound:
+            return None
