b55df3573f0decb3bbde1b3523939e786ef6fc7d
AdonyasG <adonyas2000@gmail.com>
2023-05-10 09:03:37 -0400

    end point to register users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,28 @@
 """Module app"""
 
 from flask import Flask, jsonify, request, abort, redirect
-
+from auth import Auth
 
 app = Flask(__name__)
+Auth = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
     """index"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """endpoint to register users"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = Auth.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
