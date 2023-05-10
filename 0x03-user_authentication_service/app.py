f803f4b98829d61cac4aaabcdb07d0f93e5d6757
AdonyasG <adonyas2000@gmail.com>
2023-05-10 14:42:58 -0400

    fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -40,26 +40,27 @@ def login() -> str:
         abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout() -> str:
-    """logout endpoint"""
-    session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
-        abort(403)
-
-
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
-    """profile endpoint"""
+    """profile endpoint for user"""
     session_id = request.cookies.get('session_id')
 
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """logout endpoint for user"""
+    session_id = request.cookies.get('session_id')
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
     else:
         abort(403)
 
